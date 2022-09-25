c6b3e64ee8aefe3ca050adb0d79219bd6ec78954
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-09-25 15:27:54 +0600

    Main view displays a responsive table of customers

---
+++ 1. Create a Simple Application/zhoui5/webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(
+	Controller
+) {
+	"use strict";
+
+	return Controller.extend("ai.clouddna.training00.zhoui5.controller.Main", {
+	});
+});
